be98629d2d8090b55aa298e9c7ba1896d9749fa6
vivekkushwaha23 <vk2672108@gmail.com>
2024-02-22 17:18:56 +0530

    javascript2

---
+++ 0_variables.js
@@ -0,0 +1,25 @@
+//  # In javacript ,values and variables are fundamental  concept that form the basis of programming.
+//  #values: A value is a piece of information that program can work with.it can be a number ,text,true ,false,or more complex data .A
+
+//  # variable : A variable is a container that holds a value .it has a name and can be used to store and mainuplate data in a program.
+
+var myAge=21;
+console.log(myAge);
+
+var  my_firstName ="john"
+console.log(my_firstName)
+
+
+var _myLastNames$ ="vivek"
+
+console.log(_myLastNames$);
+
+
+var $cityName="New york";
+
+console.log($cityName);
+
+
+// var 123myAge =25;
+// explain- this is not a valid variable name .it start  with a dollar sig and the subsequent Character include letter .follows javascript naming rules
+

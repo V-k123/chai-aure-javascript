d4dd3e14c94a841554bf30117422d63a0916e19f
vivekkushwaha23 <vk2672108@gmail.com>
2024-02-23 10:37:31 +0530

    javascript2

---
+++ _concat_coericon.js
@@ -0,0 +1,18 @@
+const str ="hello " + "world";
+console.log(str);
+
+console.log(10 +"20");
+console.log(9-"5");
+console.log("java" + "script");
+console.log(" " + " ");
+
+
+let sum= " " + 0;
+console.log( typeof sum);
+
+console.log("vivek" - "kushwaha");
+console.log(true+ true);
+console.log(true+ false);
+console.log(false-true);
+
+
